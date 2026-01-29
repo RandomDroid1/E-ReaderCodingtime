3e85c9e0c7017cebeae00c42929753e3a11ccdc4
RandomDroid1 <lilspi321@gmail.com>
2026-01-28 20:12:45 -0800

    Health bar, + kindle screen size area at center

---
+++ main.js
@@ -0,0 +1,6 @@
+var flipped = false;
+
+function buttonClick() { // Praise stack overflow
+    let health = document.getElementById("health")
+    health.value -= 10; // Yoinking small chunks (what the hell does this mean I dont remember writing it)
+    }
