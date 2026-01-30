1704869c7a4e61538111d74a52f2ce88a432d582
RandomDroid1 <lilspi321@gmail.com>
2026-01-29 19:30:29 -0800

    Added a start button that lets the enemy attack you, as well as a player health and heal button/. Trying to figure out how to make the setinterval stop when I want it to, its being rude rn I gota say

--- main.js
+++ main.js
@@ -1,10 +1,36 @@
 var flipped = false;
 
 function SimpleAttack() { // Praise stack overflow
-    let health = document.getElementById("health")
-    health.value -= 90; // Yoinking small chunks (what the hell does this mean I dont remember writing it)
-    if (health.value < 1) {
-        health.value = 100
+    let Ehealth = document.getElementById("EnemyHealth")
+    Ehealth.value -= Math.floor(Math.random() * 10); // Yoinking small chunks (what the hell does this mean I dont remember writing it)
+    if (Ehealth.value < 1) {
+        Ehealth.value = 100
     }
     }
 
+function SimpleHeal() {
+    let PlayerHealth = document.getElementById("PlayerHealth")
+    PlayerHealth.value += Math.floor(Math.random())* 10;
+}
+
+function StartAttack() {
+    var StartAttack = setInterval(EnemyAttack, 1000)
+    console.log("Started")
+}
+
+function EnemyAttack() {
+    let PlayerHealth = document.getElementById("PlayerHealth")
+    
+    if (PlayerHealth.value <= 0) {
+        console.log("UserDead");
+        clearInterval(StartAttack);
+    } else {
+        PlayerHealth.value -= 25;
+        console.log("attacked");
+    }
+    
+}
+
+
+
+doWork();
