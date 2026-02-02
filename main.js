3e00845be0afab56fc4a19e82350799485582c7f
RandomDroid1 <lilspi321@gmail.com>
2026-02-01 22:27:46 -0800

    Added a attack meter for the player, not functional yet. Also added an attack log for the enemy.  Made the Player Health bar look like the enemy health bar.

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-var flipped = false;
+var PlayerAttackNumber = 0;
+var EnemyAttackNumber = 0;
 let EnemyAttackMeter = document.getElementById("EnemyAttackMeter");
 document.addEventListener('DOMContentLoaded', (event) => { // Just yoink a bit of code from stack overflow and now it does what I want!
     console.log('DOM fully loaded and parsed');
@@ -7,23 +8,25 @@ document.addEventListener('DOMContentLoaded', (event) => { // Just yoink a bit o
 function StartButton() { // Starts the enemies attack when clicked
     console.log("Started");
     setTimeout(EnemyAttack, 4000);
-    setTimeout(CheckPlayerHealth, 1000);
+    setTimeout(CheckPlayerHealth, 500);
     EnemyAttackMeterTiming();
+    PlayerAttackMeterTiming();
     document.getElementById("StartButton").setAttribute('disabled','disabled'); // IT WORKS IT WORKS
 }
 
 function CheckPlayerHealth() { // repeatedly checks player health to ensure that our guy hasnt died!!
     let PlayerHealth = document.getElementById("PlayerHealth");    
-    const CheckPlayerHealthVar = setTimeout(CheckPlayerHealth, 1000);
-    console.log("PlayerALive")
+    const CheckPlayerHealthVar = setTimeout(CheckPlayerHealth, 500);
     CheckPlayerHealthVar;
     if (PlayerHealth.value <= 0) {
         clearTimeout(CheckPlayerHealthVar)
-        console.log("Cleared")
+        console.log("Cleared, player dead")
+    } else {
+        console.log ("PlayerAlive")
     }
 }
 
-// ####### PLAYER ATTACKS ####### //
+// ####### PLAYER ATTACKS ####### // 
 
 function SimpleAttack() { // Praise stack overflow
     var AttackAvaliable = false
@@ -39,19 +42,40 @@ function SimpleHeal() {
     PlayerHealth.value += 20;
 }
 
+function PlayerAttackMeterTiming() {
+    let EnemyHealth = document.getElementById("EnemyHealth")
+    if (EnemyHealth.value <= 0) {
+
+    } else { 
+    setTimeout(PlayerAttackMeterUpdate, 1000);
+    setTimeout(PlayerAttackMeterUpdate, 2000);
+    setTimeout(PlayerAttackMeterUpdate, 3000);
+    setTimeout(PlayerAttackMeterReset, 4000);}
+}
+
+function PlayerAttackMeterUpdate() {
+    let PlayerAttackMeter = document.getElementById("PlayerAttackMeter");
+    PlayerAttackMeter.value -= 25
+}
+function PlayerAttackMeterReset() {
+    let PlayerAttackMeter = document.getElementById("PlayerAttackMeter");
+    PlayerAttackMeter.value = 100
+}
 
 // ####### ENEMY ATTACK ####### //
 function EnemyAttack() {
-    let PlayerHealth = document.getElementById("PlayerHealth");
+    let PlayerHealth = document.getElementById("PlayerHealth"); // I tried to change this to write it out better and it broke so we are not changing it anymore
     
     if (PlayerHealth.value <= 0) {
-        console.log("UserDead");
         const GameStatus = document.getElementById("GameOver");
-        GameStatus.innerHTML = "test"
+        GameStatus.innerHTML = "Game over!"
     } else {
-        PlayerHealth.value -= 75;
+        EnemyAttackNumber += 1;
+        const EnemyAttackNumberText = document.getElementById("EnemyAttackLog");
+        EnemyAttackNumberText.innerHTML = "Enemy attacked with Claw! <br> 25 Damage Dealt (" + EnemyAttackNumber + ")"; 
+        PlayerHealth.value -= 25;
         setTimeout(EnemyAttack, 4000);
-        EnemyAttackMeterTiming()
+        EnemyAttackMeterTiming();
         console.log("attacked");
     }
     
@@ -60,7 +84,7 @@ function EnemyAttackMeterTiming() {
     let PlayerHealth = document.getElementById("PlayerHealth")
     if (PlayerHealth.value <= 0) {
 
-    } else { // I cannot possibly be doing this right, will this introduce like 1ms of delay over time that will haunt me later? probably?? Not my problem.
+    } else { // I cannot possibly be doing this right, will this introduce like 1ms of delay over time that will haunt me later? probably?? Not my problem. Just like the fnaf world migration glitch
     setTimeout(EnemyAttackMeterUpdate, 1000);
     setTimeout(EnemyAttackMeterUpdate, 2000);
     setTimeout(EnemyAttackMeterUpdate, 3000);
