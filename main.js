d11223b8a8abdebe2b544cd84dce655be57adc2b
RandomDroid1 <lilspi321@gmail.com>
2026-02-10 22:31:44 -0800

    Added some more CSS, making the select button work and have proper images. Replaced every instance of player with ally as thats what Im using now (Will that cause issues later, probably, but I have to redo a lot anyway so who cares?)

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
-var PlayerAttackNumber = 0;
-var EnemyClawNumber = 0;
-var EnemyStrikeNumber = 0;
-var EnemyHealNumber = 0;
+var AllyAttackNumber = 0;
 var SimpleAttackSelected = false;
 var SimpleHealSelected = false;
 var GameIsOver = false;
 var CharacterSelected = "none";
+var FactionSelected = "none";
+var AllyOneSelected = false;
+var EnemyOneSelected = false;
+var AllyTwoSelected = false;
+var EnemyTwoSelected = false;
+var AllyOne = "none";
+var AllyTwo = "none";
+var EnemyOne = "none";
+var EnemyTwo = "none";
 function VariableUpdater() {
         var EnemyAttackMeter = document.getElementById("EnemyAttackMeter");
-        var EnemyHealth = document.getElementById("EnemyHealth")
-        var PlayerHealth = document.getElementById("PlayerHealth")
+        var EnemyOneHealth = document.getElementById("EnemyHealth")
+        var AllyHealth = document.getElementById("AllyHealth")
         var GameOver = document.getElementById("GameOver");
-        var PlayerAttackMeter = document.getElementById("PlayerAttackMeter");
+        var AllyAttackMeter = document.getElementById("AllyAttackMeter");
         var EnemyAttackMeter = document.getElementById("EnemyAttackMeter");
  
  }
@@ -22,22 +28,34 @@ document.addEventListener('DOMContentLoaded', (event) => { // Just yoink a bit o
     console.log('DOM fully loaded and parsed');
 });
 // ####### START & consistent Checks ####### //
+function StartGame() {
+    if (AllyTwoSelected == true && EnemyTwoSelected == true){
+    location.assign("FightScreen.html")}
+}
+
 function StartButton() { // Starts the enemies attack when clicked
     console.log("Started");
     setTimeout(CheckHealth, 10); // is this ethical to 1ms timeout?
     EnemyAttackMeterTiming();
-    PlayerAttackMeterTiming();
+    AllyAttackMeterTiming();
     document.getElementById("StartButton").setAttribute('disabled','disabled'); // IT WORKS IT WORKS
+    if (AllyOne = "none") {
+        AllyOne = "AngelCat"
+        AllyTwo = "Boat"
+        EnemyOne = "DevilCat"
+        EnemyTwo = "Blob"
+        console.log("Test")
+    }
 }
 
 function GameOver() {
     VariableUpdater()
     if (EnemyHealth.value <= 0) {
         GameOver.innerHTML = "Game over! You Won";
-    } else if (PlayerHealth.value <= 0) {
+    } else if (AllyHealth.value <= 0) {
         GameOver.innerHTML = "Game over! You Lost ):<";
         console.log("GameLostRan")
-    } else if (EnemyHealth.value <= 0 && PlayerHealth.value <= 0) {
+    } else if (EnemyHealth.value <= 0 && AllyHealth.value <= 0) {
         GameOver.innerHTML = "Game over! You ALL LOSE";
     };
     document.getElementById("AttackButton").setAttribute('disabled','disabled');
@@ -46,12 +64,12 @@ function GameOver() {
     console.log("GameOver func ran")
 
 }
-function CheckHealth() { // Checks health of player and enemy, does needed updates and shutdowns
-    if (PlayerHealth.value > 0 && EnemyHealth.value  > 0) {
+function CheckHealth() { // Checks health of Ally and enemy, does needed updates and shutdowns
+    if (AllyHealth.value > 0 && EnemyHealth.value  > 0) {
         console.log("Both Alive")
         setTimeout(CheckHealth,100)
-    } else if (PlayerHealth.value <= 0) {
-        console.log("PlayerDead")
+    } else if (AllyHealth.value <= 0) {
+        console.log("AllyDead")
         GameOver()
     } else if (EnemyHealth.value <= 0) {
         console.log("EnemyDead")
@@ -61,7 +79,7 @@ function CheckHealth() { // Checks health of player and enemy, does needed updat
 
 
 
-// ####### PLAYER ATTACKS ####### // 
+// ####### Ally ATTACKS ####### // 
 
 function SimpleAttack() { // Praise stack overflow
     if (SimpleHealSelected == false) {
@@ -79,49 +97,49 @@ function SimpleHeal() {
     }
 }
 
-function PlayerAttackMeterTiming() {
+function AllyAttackMeterTiming() {
     VariableUpdater()
-    if (EnemyHealth.value <= 0 || PlayerHealth.value <= 0) {
+    if (EnemyHealth.value <= 0 || AllyHealth.value <= 0) {
 
     } else { 
-    setTimeout(PlayerAttackMeterUpdate, 1000);
-    setTimeout(PlayerAttackMeterUpdate, 2000);
-    setTimeout(PlayerAttackMeterUpdate, 3000);
-    setTimeout(PlayerAttackMeterAct, 4000);}
+    setTimeout(AllyAttackMeterUpdate, 1000);
+    setTimeout(AllyAttackMeterUpdate, 2000);
+    setTimeout(AllyAttackMeterUpdate, 3000);
+    setTimeout(AllyAttackMeterAct, 4000);}
 }
 
-function PlayerAttackMeterUpdate() {
+function AllyAttackMeterUpdate() {
     VariableUpdater()
-    PlayerAttackMeter.value -= 25
+    AllyAttackMeter.value -= 25
 }
-function PlayerAttackMeterAct() {
+function AllyAttackMeterAct() {
     VariableUpdater()
     if (SimpleAttackSelected == true) {
-        PlayerAttackNumber =+ 1;
-        var PlayerAttackDamage = Math.floor(Math.random() * (75-10+1));
+        AllyAttackNumber =+ 1;
+        var AllyAttackDamage = Math.floor(Math.random() * (75-10+1));
         SimpleAttackSelected = false;
-        const PlayerAttackNumberText = document.getElementById("PlayerAttackLog");
-        PlayerAttackNumberText.innerHTML = "Player attacked! <br>" + PlayerAttackDamage + " Damage Dealt (" + PlayerAttackNumber + ")";
-        EnemyHealth.value -= PlayerAttackDamage;
+        const AllyAttackNumberText = document.getElementById("AllyAttackLog");
+        AllyAttackNumberText.innerHTML = "Ally attacked! <br>" + AllyAttackDamage + " Damage Dealt (" + AllyAttackNumber + ")";
+        EnemyHealth.value -= AllyAttackDamage;
         document.getElementById("AttackButton").style.borderWidth = "1px";
         document.getElementById("AttackButton").style.borderColor = "black";
-        console.log("PlayerAttacked");
+        console.log("AllyAttacked");
         
         }
-    if (SimpleHealSelected == true && PlayerHealth.value > 0) {
-        PlayerHealNumber =+ 1;
-        var PlayerHealAmount = Math.floor(Math.random() * (50-10+1));
+    if (SimpleHealSelected == true && AllyHealth.value > 0) {
+        AllyHealNumber =+ 1;
+        var AllyHealAmount = Math.floor(Math.random() * (50-10+1));
         SimpleAttackSelected = false;
-        const PlayerHealNumberText = document.getElementById("PlayerAttackLog");
-        PlayerHealNumberText.innerHTML = "Player Healed! <br>" + PlayerHealAmount + " Health Healed (" + PlayerHealNumber + ")";
-        PlayerHealth.value += PlayerHealAmount;
+        const AllyHealNumberText = document.getElementById("AllyAttackLog");
+        AllyHealNumberText.innerHTML = "Ally Healed! <br>" + AllyHealAmount + " Health Healed (" + AllyHealNumber + ")";
+        AllyHealth.value += AllyHealAmount;
         SimpleHealSelected = false
         document.getElementById("HealButton").style.borderWidth = "1px";
         document.getElementById("HealButton").style.borderColor = "black";
-        console.log("PlayerHeal")
+        console.log("AllyHeal")
         }
-    PlayerAttackMeter.value = 100
-    PlayerAttackMeterTiming()
+    AllyAttackMeter.value = 100
+    AllyAttackMeterTiming()
 }
 // ####### ENEMY ONE MOVE HANDLER ####### //
     function EnemyOneAttackSelector() {
@@ -134,30 +152,30 @@ function PlayerAttackMeterAct() {
 
 function EnemyOneAttackMeterTiming() {
     VariableUpdater()
-    if (EnemyHealth.value <= 0 || PlayerHealth.value <= 0) {
+    if (EnemyHealth.value <= 0 || AllyHealth.value <= 0) {
 
     } else { // I cannot possibly be doing this right, will this introduce like 1ms of delay over time that will haunt me later? probably?? Not my problem. Just like the fnaf world migration glitch :(
-    setTimeout(EnemyAttackMeterUpdate, 1000);
-    setTimeout(EnemyAttackMeterUpdate, 2000);
-    setTimeout(EnemyAttackMeterUpdate, 3000);
-    setTimeout(EnemyAttackMeterReset, 4000);
+    setTimeout(EnemyOneAttackMeterUpdate, 1000);
+    setTimeout(EnemyOneAttackMeterUpdate, 2000);
+    setTimeout(EnemyOneAttackMeterUpdate, 3000);
+    setTimeout(EnemyOneAttackMeterReset, 4000);
     console.log(EnemyHealth.value);
-    console.log(PlayerHealth.value);
+    console.log(AllyHealth.value);
 }
 }
 function EnemyOneAttackMeterUpdate() {
     VariableUpdater()
-    if (PlayerHealth.value <= 0 || EnemyHealth.value <= 0) {
+    if (AllyHealth.value <= 0 || EnemyHealth.value <= 0) {
        console.log(EnemyHealth.value);
-       console.log(PlayerHealth.value);
+       console.log(AllyHealth.value);
     } else {
     EnemyAttackMeter.value -= 25
     }
 }
 function EnemyOneAttackMeterReset() {
-     if (PlayerHealth.value <= 0 || EnemyHealth.value <= 0) {
+     if (AllyHealth.value <= 0 || EnemyHealth.value <= 0) {
        console.log(EnemyHealth.value);
-       console.log(PlayerHealth.value);
+       console.log(AllyHealth.value);
     } else {
     VariableUpdater()
     EnemyAttackMeter.value = 100
@@ -165,7 +183,7 @@ function EnemyOneAttackMeterReset() {
     }
     
 }
-// ####### PlayerAttacks ####### //
+// ####### AllyAttacks ####### //
 
 function HeavenlyStrike() {
     enemyattack = 0
@@ -188,10 +206,39 @@ function UnselectAll() {
 }
 
 function SelectionButton() {
-
+    if (FactionSelected == "Ally") {
+        if (AllyOneSelected == false) {
+            document.getElementById("AllySelectionPortraitOne").src = CharacterSelected+"HeadshotUnselected.png"
+            AllyOneSelected = true
+            AllyOne = CharacterSelected
+        }
+        if (AllyOneSelected == true && AllyTwoSelected == false) {
+            if (AllyOne != CharacterSelected) {
+            document.getElementById("AllySelectionPortraitTwo").src = CharacterSelected+"HeadshotUnselected.png"
+            AllyTwoSelected = true
+            AllyTwo = CharacterSelected
+        }
+        }
+    }
+    if (FactionSelected == "Enemy") {
+        if (EnemyOneSelected == false) {
+            document.getElementById("EnemySelectionPortraitOne").src = CharacterSelected+"HeadshotUnselected.png"
+            EnemyOneSelected = true
+            EnemyOne = CharacterSelected
+        }
+        if (EnemyOneSelected == true && EnemyTwoSelected == false) {
+            if (EnemyOne != CharacterSelected) {
+            document.getElementById("EnemySelectionPortraitTwo").src = CharacterSelected+"HeadshotUnselected.png"
+            EnemyTwoSelected = true
+            EnemyTwo = CharacterSelected
+        }
+        }
+    }
 }
 function AngelCatHeadshot() {
     UnselectAll()
+    CharacterSelected = "AngelCat";
+    FactionSelected = "Ally"
     document.getElementById("AngelCatHeadshotImage").src = "AngelCatHeadshotSelected.png";
     document.getElementById("CharacterName").innerText = "AngelCat"
     document.getElementById("Tagline").innerText = "I will be your angle (90 degrees)"
@@ -204,6 +251,8 @@ function AngelCatHeadshot() {
 }    
 function BlobHeadshot() {
     UnselectAll()
+    CharacterSelected = "Blob";
+    FactionSelected == "Enemy";
     document.getElementById("BlobHeadshotImage").src = "BlobHeadshotSelected.png";
     document.getElementById("CharacterName").innerText = "Amorphous Blob"
     document.getElementById("Tagline").innerText = "Gleep Glorp Zeep Zip"
@@ -217,6 +266,8 @@ function BlobHeadshot() {
 
 function ClockHeadshot() {
     UnselectAll()
+    CharacterSelected = "Clock";
+    FactionSelected = "Ally";
     document.getElementById("ClockHeadshotImage").src = "ClockHeadshotSelected.png";
     document.getElementById("CharacterName").innerText = "Tovar"
     document.getElementById("Tagline").innerText = "Is it really the right time for this?"
@@ -230,6 +281,8 @@ function ClockHeadshot() {
 
 function SlugcatHeadshot() {
     UnselectAll()
+    CharacterSelected = "Slugcat";
+    FactionSelected = "Ally";
     document.getElementById("SlugcatHeadshotImage").src = "SlugcatHeadshotSelected.png";
     document.getElementById("CharacterName").innerText = "The Watcher"
     document.getElementById("Tagline").innerText = "Blatant copyright infrigement"
@@ -243,6 +296,8 @@ function SlugcatHeadshot() {
 
 function SquareHeadshot() {
     UnselectAll()
+    CharacterSelected = "Square";
+    FactionSelected = "Ally";
     document.getElementById("SquareHeadshotImage").src = "SquareHeadshotSelected.png";
     document.getElementById("CharacterName").innerText = "Square"
     document.getElementById("Tagline").innerText = "They said be there or be square, \nhe wasn't there"
@@ -256,6 +311,8 @@ function SquareHeadshot() {
 }    
 function BoatHeadshot() {
     UnselectAll()
+    CharacterSelected = "Boat";
+    FactionSelected = "Ally";
     document.getElementById("BoatHeadshotImage").src = "BoatHeadshotSelected.png";
     document.getElementById("CharacterName").innerText = "Boat"
     document.getElementById("Tagline").innerText = "Boat"
@@ -269,6 +326,8 @@ function BoatHeadshot() {
 
 function FlibbitHeadshot() {
     UnselectAll()
+    CharacterSelected = "Flibbit";
+    FactionSelected = "Ally";
     document.getElementById("FlibbitHeadshotImage").src = "FlibbitHeadshotSelected.png";
     document.getElementById("CharacterName").innerText = "Flibbit"
     document.getElementById("Tagline").innerText = "Get a load of this guy"
@@ -282,20 +341,24 @@ function FlibbitHeadshot() {
 
 function DevilCatHeadshot() {
     UnselectAll()
+    CharacterSelected = "DevilCat";
+    FactionSelected = "Enemy";
     document.getElementById("DevilCatHeadshotImage").src = "DevilCatHeadshotSelected.png";
     document.getElementById("CharacterName").innerText = "DevilCat"
     document.getElementById("Tagline").innerText = "The devil to your angle (180 degrees)"
     document.getElementById("Tagline").style.fontSize = "medium"
     document.getElementById("AttackOne").innerText = "Demonic Strike (UPG)"
-    document.getElementById("AttackOneText").innerText = "Strike your target down with\nhigh damage to one player"
+    document.getElementById("AttackOneText").innerText = "Strike your target down with\nhigh damage to one Ally"
     document.getElementById("AttackTwo").innerText = "Damning"
-    document.getElementById("AttackTwoText").innerText="Deal Medium damage to all \nplayers"
+    document.getElementById("AttackTwoText").innerText="Deal Medium damage to all \nAllys"
     document.getElementById("AttackThree").innerText="Fire Blast"
-    document.getElementById("AttackThreeText").innerText="Deals significant fire \ndamage to players"
+    document.getElementById("AttackThreeText").innerText="Deals significant fire \ndamage to Allys"
 }    
 
 function CarHeadshot() {
     UnselectAll()
+    CharacterSelected = "Car";
+    FactionSelected = "Enemy";
     document.getElementById("CarHeadshotImage").src = "CarHeadshotSelected.png";
     document.getElementById("CharacterName").innerText = "Dashboard"
     document.getElementById("Tagline").innerText = "Don't get ran over!"
@@ -309,19 +372,23 @@ function CarHeadshot() {
 
 function OrigamiHeadshot() {
     UnselectAll()
+    CharacterSelected = "Origami";
+    FactionSelected = "Enemy";
     document.getElementById("OrigamiHeadshotImage").src = "OrigamiHeadshotSelected.png";
     document.getElementById("CharacterName").innerText = "Crane"
     document.getElementById("Tagline").innerText = "Still can't fold one IRL"
     document.getElementById("AttackOne").innerText = "Fold"
     document.getElementById("AttackOneText").innerText = "Does 10 damage, \ninfinitely upgradeable"
     document.getElementById("AttackTwo").innerText = "Peck"
-    document.getElementById("AttackTwoText").innerText="Does medium damage to a \nrandom player"
+    document.getElementById("AttackTwoText").innerText="Does medium damage to a \nrandom Ally"
     document.getElementById("AttackThree").innerText=""
     document.getElementById("AttackThreeText").innerText=""
 }    
 
 function DNAHeadshot() {
     UnselectAll()
+    CharacterSelected = "DNA";
+    FactionSelected = "Enemy";
     document.getElementById("DNAHeadshotImage").src = "DNAHeadshotSelected.png";
     document.getElementById("CharacterName").innerText = "Helix" 
     document.getElementById("Tagline").innerText = "Hopefully prion free"
@@ -335,6 +402,8 @@ function DNAHeadshot() {
 
 function HandManHeadshot() {
     UnselectAll()
+    CharacterSelected = "HandMan";
+    FactionSelected = "Enemy";
     document.getElementById("HandManHeadshotImage").src = "HandManHeadshotSelected.png";
     document.getElementById("CharacterName").innerText = "Evil"
     document.getElementById("Tagline").innerText = "I don't even know man"
